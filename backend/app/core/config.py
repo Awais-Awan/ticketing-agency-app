d6a7f3aef2a64246040c3f7ae49ea92b80aa53de
Awais-Awan <awanawais400@gmail.com>
2026-08-30 14:47:00 +0500

    creating database tables and alembic setup

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,8 @@
-from pydantic-settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    model_config = SettingsConfigDict(env_file=".env")
     
-    class config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
         
 settings = Settings()
